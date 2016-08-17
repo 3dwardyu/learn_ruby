8c03e983d2f2a9689bb1f96ccaf75290f8d847a0
Edward Yu <3dward.yu@gmail.com>
2016-08-16 22:55:40 -0400

    create and tested simon says methods

---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,40 @@
+
+def echo(repeat)
+  repeat
+end
+
+def shout(word)
+  word.upcase!
+end
+
+def repeat(word, num = 2)
+  repeat = ""
+  num.times do repeat += word
+    repeat += ' '
+  end
+  return repeat.strip
+end
+
+def start_of_word(word, num)
+  word[0,num]
+end
+
+def first_word(string)
+  string.split.first
+end
+
+def titleize(string)
+array = string.split
+array.each do |word|
+
+  if word != "and" && word != "the" && word != "over"
+    puts word
+    word.capitalize!
+    puts word
+  else
+    word.downcase
+  end
+end
+    array[0].capitalize!
+    return array.join(" ")
+ end
